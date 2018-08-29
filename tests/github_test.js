90db73cc4a86fb7aa8212adedb2cb5b72b4cdb6e
Germán Giunta <germanfgiunta@gmail.com>
2018-08-28 22:28:15 -0300

    config file implementation

--- tests/github_test.js
+++ tests/github_test.js
@@ -1,16 +1,16 @@
 Feature('Github tests');
 
-Scenario('Login', (I, loginPage, dashboardPage) => {
+Scenario('Login', (I, config, loginPage, dashboardPage) => {
     I.amOnPage(loginPage.path);
     I.waitForElement(loginPage.signInlabel);
-    loginPage.submitCredentials('ggiunta', 'D4rkM4tt3r')
+    loginPage.submitCredentials(config.validUser.user, config.validUser.password)
     I.waitForElement(dashboardPage.exploreGithubLabel)
 });
 
-Scenario('Logout', (I, loginPage, dashboardPage, homePage) => {
+Scenario('Logout', (I, config, loginPage, dashboardPage, homePage) => {
     I.amOnPage(loginPage.path);
     I.waitForElement(loginPage.signInlabel);
-    loginPage.submitCredentials('ggiunta', 'D4rkM4tt3r')
+    loginPage.submitCredentials(config.validUser.user, config.validUser.password)
     I.waitForElement(dashboardPage.exploreGithubLabel)
     I.click(dashboardPage.avatar)
     I.click(dashboardPage.avatarMenu.signOut)
